7e06cc474573c354ecc16965ae7c96a2c9afe93a
tang <1585701160@qq.com>
2019-12-26 17:26:52 +0800

    正则路由1

--- demo006.py
+++ demo006.py
@@ -1,13 +1,22 @@
 """
-正则路由
+正则路由1
 """
 from flask import Flask
+# 0.导入相关的模块
 from werkzeug.routing import BaseConverter
 
 app = Flask(__name__)
 
 
-# 自定义转换器
+# 1.自定义转换器
+class RegexConverter(BaseConverter):
+	"""自定义转换器"""
+
+	# 修改类的regex属性
+	regex = "[0-9]{6}"
+
+# 2.将自定义的转换器添加到系统默认的转换器列表中
+app.url_map.converters['re'] = RegexConverter
 
 
 @app.route('/')
@@ -17,7 +26,7 @@ def index():
 
 #
 
-@app.route('/user/<user_id>')
+@app.route('/user/<re:user_id>')
 def user(user_id):
 	return user_id
 
