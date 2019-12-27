c9a60140d6ce51b11edf334373bbc42921a03fe8
tang <1585701160@qq.com>
2019-12-27 10:55:46 +0800

    正则路由there

--- demo008.py
+++ demo008.py
@@ -1,3 +1,15 @@
 """
 正则转换器3to_python
 """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+	return "index"
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
