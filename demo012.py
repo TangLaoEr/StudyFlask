6d8b356d8f79c7def4959dee4932cc7db2c7acbb
tang <1585701160@qq.com>
2019-12-28 22:52:38 +0800

    session

--- demo012.py
+++ demo012.py
@@ -1,17 +1,27 @@
 """
-session
-
+session 状态保持
 """
-from flask import Flask, request, make_response, sessions
+from flask import Flask, request, make_response, session
 
 app = Flask(__name__)
 
+app.secret_key = "tanglaoer"
 
 @app.route('/')
 def index():
 	return "index"
 
+@app.route('/login')
+def login():
+	"""login"""
+	session['user_id'] = 1
+	session['user_name'] = 'laowang'
+	return "login"
 
+
+@app.route('/logout')
+def logout():
+	return "logout"
 
 
 if __name__ == '__main__':
