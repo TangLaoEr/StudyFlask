972987dab6ed3c762549cfe33e10a064029b5689
tang <1585701160@qq.com>
2019-12-28 17:50:16 +0800

    cookie and session

--- demo011.py
+++ demo011.py
@@ -1,7 +1,7 @@
 """
 cookie and session
 """
-from flask import Flask, sessions, Request, Response, make_response
+from flask import Flask, sessions, make_response, request
 
 app = Flask(__name__)
 
@@ -20,17 +20,23 @@ app = Flask(__name__)
 @app.route("/login")
 def login():
 	"""登录函数"""
-
 	# 生成一个响应的函数
 	response = make_response('success')
-	response.set_cookie('user_id', '1')
+	response.set_cookie('user_id', '1', max_age=3600)  # 秒为单位
 	response.set_cookie('user_name', 'laowang')
 	return response
 
 
+
 @app.route('/')
 def index():
+	"""首页"""
 
+	# 在请求中获取cookie
+	user_id = request.cookies.get('user_id')
+	user_name = request.cookies.get('user_name')
+	print(user_id)
+	print(user_name)
 	return "index"
 
 if __name__ == '__main__':
