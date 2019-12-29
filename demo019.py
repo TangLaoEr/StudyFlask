81bd2eac8c8f6a93f89652f8c4b0ff1f46de0426
tang <1585701160@qq.com>
2019-12-29 19:02:51 +0800

    过滤器

--- demo019.py
+++ demo019.py
@@ -11,15 +11,19 @@ def index():
 	user = 'user'
 	username = 'tanglaoer'
 	my_list = [1, 2, 3, 4]
+	my_list_str = ['tang', 'chen', 'li', 'lao']
 	my_dict = {
 		"id": '1',
 		"name": "laowang"
 	}
+	my_html = "<h1>html代码</h1>"
 	return render_template('code.html',
 							user=user,
 							username=username,
 							my_list=my_list,
-							my_dict=my_dict
+							my_dict=my_dict,
+						   	my_html = my_html,
+							my_list_str=my_list_str,
 							)
 
 
