cc97c978bc705b9d7a37952e8d72d38e732e6aba
tang <1585701160@qq.com>
2020-01-11 15:48:32 +0800

    数据库迁移操作命令

--- demo029.py
+++ demo029.py
@@ -3,24 +3,40 @@
 """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+# MigrateCommand 迁移的命令
+# 需要借助flask_script 库作用
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://tang:123456@47.106.82.47:3306/StudentFlask'
 app.config["SQLALCHEMY_COMMIT_ON_TEARDOWN"] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.debug = True
 
 db = SQLAlchemy(app)
 
+# 使用迁移类将应用和数据库连接对象保存起来
+Migrate(app, db)
+# 创建终端命令的对象
+manager = Manager(app)
+# 将数据库的迁移命令添加到manager中, 一般是db
+manager.add_command('db', MigrateCommand)
 
 class Role(db.Model):
 	__tablename__ = "Role"
 	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(64), unique=True)
+	nick_name = db.Column(db.String(64), unique=True)
 	# backref 在这行代码的作用是：给前面的 User添加一个属性， 名字叫backref的值
 	# 以便可以直接通过user.role方法到一 的一方的数据
 
 	# 一跟多的互相访问
 	users = db.relationship('User', backref='role')
+
+	# 新增字段
+	title = db.Column(db.String(64))
+	heigth = db.Column(db.Integer)
 
 	"""
 	user = User.query.get(1)
@@ -52,20 +68,28 @@ def index():
 
 
 if __name__ == '__main__':
-	db.drop_all()
-	db.create_all()
-	ro1 = Role(name='admin')
-	ro2 = Role(name='user')
-	db.session.add_all([ro1, ro2])
-	db.session.commit()
+	manager.run()
 
-	user1 = User(name='laowang')
-	user1.role_id = 1
-	user2 = User(name='laoli')
-	user2.role_id = 1
-	user3 = User(name='laozhang')
-	db.session.add_all([user1, user2, user3])
+"""
+$python demo029.py aaa init   # aaa 是自己定义的， 会创建一个目录migrations
+$python demo029.py aaa migrate -m"initial"
 
-	# 提交
-	db.session.commit()
-	app.run(debug=True)
+总结迁移的命令
+db = manager.add_command('db', MigrateCommand)
+1、迁移初始化 python xxx.py db init  # 只有第一次的时候需要
+
+2、生成迁移版本文件 python xxx.py db migrate -m "xxxx"  # 迁移生成并记录log
+3、执行迁移(往上迁移) python xxx.py db upgrate    # 这里才会生成表
+
+# 案例一 添加字段需要 执行 2 3 步骤
+python xxx.py db migrate -m "xxx"
+python xxx.py db upgrade 
+
+
+命令行命令大全：
+python 文件 db init
+python 文件 db migrate -m "版本名（注释）"
+python 文件 db upgrade 然后观察表结构
+python 文件 db history 查看版本号
+python 文件 db downgrade
+"""
